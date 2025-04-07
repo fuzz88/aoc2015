67af6180d77efa147c2f35cbeee1475f82de76f0
fuzz88 <ivan@oschepkov.ru>
2025-04-07 19:24:29 +0800

    [day16] part1 success. VecDeque?

--- day17/p1.rs
+++ day17/p1.rs
@@ -1,8 +1,9 @@
+use std::cmp::Ordering;
+use std::env;
 use std::fs;
 use std::io;
-use std::env;
 
-fn read_input(file_path: &str) -> Result<Vec<u32>, io::Error> {
+fn read_input(file_path: &String) -> Result<Vec<u32>, io::Error> {
     let input = fs::read_to_string(file_path)?
         .lines()
         .map(|line| line.parse().unwrap())
@@ -11,17 +12,61 @@ fn read_input(file_path: &str) -> Result<Vec<u32>, io::Error> {
     Ok(input)
 }
 
-fn main() {
+fn step_by_container(
+    solution: Vec<usize>,
+    containers: &Vec<u32>,
+    current_index: usize,
+    total_volume: u32,
+    count: &mut u32,
+) -> u32 {
+    for index in current_index..containers.len() {
+        if !solution.contains(&index) {
+            let mut solution = solution.clone();
+            solution.push(index);
+            let volume: u32 = solution.iter().map(|index| containers[*index]).sum();
+            match volume.cmp(&total_volume) {
+                Ordering::Greater => {}
+                Ordering::Equal => {
+                    *count += 1;
+                    println!(
+                        "{:?}",
+                        solution
+                            .iter()
+                            .map(|index| containers[*index])
+                            .collect::<Vec<u32>>()
+                    );
+                    println!("{:?}\n", solution);
+                }
+                Ordering::Less => {
+                    step_by_container(solution, containers, index, total_volume, count);
+                }
+            };
+        }
+    }
+    *count
+}
 
+fn count_placements(containers: Vec<u32>, total_volume: u32) -> u32 {
+    let solution: Vec<usize> = Vec::new();
+    let count: &mut u32 = &mut 0;
+    step_by_container(solution, &containers, 0, total_volume, count)
+}
+
+fn main() {
     let args: Vec<_> = env::args().collect();
     if args.len() == 3 {
         let input_file = &args[1];
         let input = read_input(input_file).unwrap();
-    
+        let volume: u32 = args[2].parse().unwrap();
+
         println!("Containers count: {}", input.len());
-        println!("Liters of eggnog: {}", args[2]);
+        println!("Liters of eggnog: {}", volume);
+
+        println!(
+            "Different combination of containers: {}",
+            count_placements(input, volume)
+        );
     } else {
         println!("ERROR: no input params");
     }
-
 }
